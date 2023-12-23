8da5a8b2bf5ae7e67d94c553b8c80300eb6a22f9
Pedro Paixao <paixaop@gmail.com>
2023-12-23 02:09:33 -0500

    day 14 part 2

--- mysolutions/2023/day14.py
+++ mysolutions/2023/day14.py
@@ -10,54 +10,100 @@ def parse(data):
 
     return parsed
 
-def slide_rocks(column):
-    
+def slide_rocks_we(data, row, check= '.', swap= 'O'): 
     while True:
         switch = False
-        for i in range(len(column) - 1):
-            if column[i] == '.':
-                if column[i+1] == 'O':
-                    column[i] = 'O'
-                    column[i+1] = '.'
+        for i in range(len(data[row]) - 1):
+            if data[row][i] == check:
+                if data[row][i+1] == swap:
+                    data[row][i] = swap
+                    data[row][i+1] = check
                     i += 1
                     switch = True
         
         if not switch:
             break
-    
+
+def slide_rocks_ns(data, col, check= '.', swap= 'O'): 
+    while True:
+        switch = False
+        for i in range(len(data) - 1):
+            if data[i][col] == check:
+                if data[i+1][col] == swap:
+                    data[i][col] = swap
+                    data[i+1][col] = check
+                    i += 1
+                    switch = True
+        
+        if not switch:
+            break
+
+def calc_weight(data):
     total = 0
-    for i in range(len(column)):
-        if column[i] == 'O':
-            total += len(column) - i
+    number_of_rows = len(data)
+    for i, row in enumerate(data):
+        for j, item in enumerate(row):
+            if item == 'O':
+                total += number_of_rows - i
 
     return total
-
-
-def remove_column(ll, n):
-    c = []
-    for row in ll:
-        for i, col in enumerate(row):
-            if i == n:
-                c.append(col)
-    return c
-
 
 def part_a(data):
     data = parse(data)
     total = 0
     
     south_edge = len(data)
-    for col in range(len(data[0])):
-        column = remove_column(data, col)
-        total += slide_rocks(column)
-        
+    
+    #data = [list(x) for x in zip(*data)]
+    for col in range(len(data)):
+        #column = remove_column(data, col)
+        slide_rocks_ns(data, col)
+    total = calc_weight(data)
     return total
+
+def north(data):
+    for col in range(len(data)):
+        slide_rocks_ns(data, col)
+
+def south(data):
+    for col in range(len(data)):
+        slide_rocks_ns(data, col, check = 'O', swap = '.')
+
+def west(data):
+    for col in range(len(data)):
+        slide_rocks_we(data, col)
+
+def east(data):
+    for col in range(len(data)):
+        slide_rocks_we(data, col, check = 'O', swap = '.')
 
 
 def part_b(data):
     data = parse(data)
     total = 0
+    
+    tdata = "\n".join("".join(row) for row in data)
+    seen = { tdata }
+    loop = [tdata]
+    i = 0
+    while True:
+        i += 1
+        north(data)
+        west(data)
+        south(data)
+        east(data)
 
+        tdata = "\n".join("".join(row) for row in data)
+        if tdata in seen:
+            break
+
+        seen.add(tdata)
+        loop.append(tdata)
+    
+    loop_start = loop.index(tdata)
+
+    data = loop[(1000000000 - loop_start) % (i - loop_start) + loop_start].splitlines()
+    total = sum(row.count("O") * (len(data) - r) for r, row in enumerate(data))
     return total
 
 test_data_part_a = """\
