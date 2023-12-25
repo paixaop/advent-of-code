bea3f120020548e6a099daf5178832d8cc962f59
Pedro Paixao <paixaop@gmail.com>
2023-12-25 02:12:44 -0500

    day 17 part 1 finished

--- mysolutions/2023/day17.py
+++ mysolutions/2023/day17.py
@@ -42,140 +42,57 @@ def aoc17(grid):
                     heappush(pq, (hl + grid[nr][nc], nr, nc, ndr, ndc, 1))
 
 
-def get_next(path, current, length= MAX_STRAIGHT):
-    arrows = []
-    n = 0
-    pos = current
-    if pos in path:
-        (curr_row, curr_col, curr_arrow) = pos
-        if (curr_row, curr_col) == (0,0): # at the start
-            return [(1, 0, "v"), (0, 1, ">")]
-        
-        while pos and n < length:
-            (r, c, arrow) = pos
-            arrows.append(arrow)
-            pos = path[pos]
-            n += 1
 
-        if n == length:
-            if arrows.count(">") == n or arrows.count("<") == n: # Moving horizontally
-                return [(curr_row-1, curr_col, "^"), (curr_row+1, curr_col, "v")] # Must go up or down
-            elif arrows.count("^") == n or arrows.count("v") == n: # Moving vertically
-                return [(curr_row, curr_col-1, "<"), (curr_row, curr_col+1, ">")] # Must go left or right
-        
-        (prev_row, prev_col, prev_arrow) = path[current]
-
-        if curr_arrow == ">": # Moving right -> can go up, down and right
-            return [(curr_row-1, curr_col, "^"), (curr_row+1, curr_col, "v"), (curr_row, curr_col+1, ">")]
-        elif curr_arrow == "<": # Moving left -> can go up, down and left
-            return [(curr_row-1, curr_col, "^"), (curr_row+1, curr_col, "v"), (curr_row, curr_col-1, "<")]
-        elif curr_arrow == "v": # Moving down -> can go down, left and right
-            return [(curr_row+1, curr_col, "v"), (curr_row, curr_col-1, "<"), (curr_row, curr_col+1, ">")]
-        else: # Moving up -> can go up, left and right
-            return [(curr_row-1, curr_col, "^"), (curr_row, curr_col-1, "<"), (curr_row, curr_col+1, ">")]
-    return []
-    
 def in_bounds(data, pos):
-    (r, c, _) = pos
+    (r, c) = pos
     return 0 <= r < len(data) and 0 <= c < len(data[0])
 
-def cost(data, pos):
-    if in_bounds(data, pos):
-        (r, c, _) = pos
-        return data[r][c]
-    return 0
-
-def neighbors(data, came_from, pos):
-    if not in_bounds(data, pos):
-        return []
-    results = get_next(came_from, pos)
-    return results
-
-def dijkstra_search2(data, start, goal, draw=True):
-    start =  (start[0], start[1], "o")
-    pq = [(0, start)]
-    came_from = {}
-    cost_so_far = {}
-    came_from[start] = None
-    cost_so_far[start] = 0
-
-    
-    while pq:
-        heat_loss, current = heappop(pq)
-        (r, c, _) = current 
-        if (r, c) == goal:
-            break
-        
-        goto = get_next(came_from, current)
-        neighbors = list(filter(lambda n: in_bounds(data, n), goto))
-        for next in neighbors:
-            new_cost = cost_so_far[current] + cost(data, next)
-            if next not in cost_so_far or new_cost < cost_so_far[next]:
-                cost_so_far[next] = new_cost
-                heappush(pq, (new_cost, next))
-                came_from[next] = current
-    
-    min_cost = 0
-    end = start
-    for arrow in ["<", ">", "^", "v"]:
-        g = (goal[0], goal[1], arrow)
-        if g in cost_so_far:
-            if end == start or min_cost > cost_so_far[g]:
-                min_cost = cost_so_far[g]
-                end = g
-
-    if draw:
-        draw_path(data, came_from, start, end)
-
-    return cost_so_far[end]
-
-    
 def dijkstra_search(data, start, goal, draw=True):
-    start =  (start[0], start[1], "o")
-    pq = [(0, start)]
+    if not in_bounds(data, start) or not in_bounds(data, goal):
+        return -1
+    
+    pq = [(0, (start[0], start[1], 0, 0, 0))]
+    
     came_from = {}
     came_from[start] = None
     seen = set()
     
     while pq:
         heat_loss, current = heappop(pq)
-        (r, c, _) = current 
-        if (r, c) == goal:
-            min_cost = heat_loss
-            break
+        (row, col, dr, dc, n) = current
+        
+        if not in_bounds(data, (row, col)):
+            continue
+        
+        if (row, col) == goal:
+            return heat_loss
         
         if current in seen:
             continue
         
         seen.add(current)
-
-        goto = get_next(came_from, current)
-        neighbors = list(filter(lambda n: in_bounds(data, n), goto))
-        for next in neighbors:
-            new_heat_loss = heat_loss + cost(data, next)
-            heappush(pq, (new_heat_loss, next))
-            came_from[next] = current
-
-    if draw:
-        draw_path(data, came_from, start, current)
-
-    return cost_so_far[end]
-
-def draw_path(data, path, start, goal):
-    d = []
-    for r in range(len(data)):
-        d.append(list("." * len(data[0])))
-    pos = goal
-    while pos != start:
-        if pos in path:
-            (r, c, a) = pos
-            next = path[pos]
-            d[r][c] = a
-            pos = next
-
-    for row in d: 
-        print("".join(list(map(str, row))))
+        if n < MAX_STRAIGHT and (dr,dc) != (0, 0):
+            next_row = row + dr
+            next_col = col + dc
+            next = (next_row, next_col, dr, dc, n + 1)
             
+            if in_bounds(data, (next_row, next_col)):
+                next_heat_loss = heat_loss + data[next_row][next_col]
+                heappush(pq, (next_heat_loss, next))
+            
+
+        neighbors = [(-1, 0), (1 , 0), (0, -1), (0, 1)]
+        for ndr, ndc in neighbors:
+            if (ndr, ndc) != (dr, dc) and (ndr, ndc) != (-dr, -dc):
+                next_row = row + ndr
+                next_col = col + ndc
+                next = (next_row, next_col, ndr, ndc, 1)
+                if in_bounds(data, (next_row, next_col)):
+                    next_heat_loss = heat_loss + data[next_row][next_col]
+                    heappush(pq, (next_heat_loss, next))
+            
+    return -1
+         
 def part_a(data):
     data = parse(data)
     total = 0
@@ -215,4 +132,4 @@ if __name__ == "__main__":
     data = common.get_data(__file__)
     
     common.run(part_a, test_data_part_a, data, 102)
-    common.run(part_b, test_data_part_b, data, 0)
+    common.run(part_b, test_data_part_b, data, 94)
