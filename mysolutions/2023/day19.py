d48a16ec93842febbef440e891a6cf8e52be1492
Pedro Paixao <paixaop@gmail.com>
2023-12-25 23:53:50 -0500

    day 19 part 1

--- mysolutions/2023/day19.py
+++ mysolutions/2023/day19.py
@@ -17,40 +17,75 @@ def parse(data):
         if m:
             m = m[0]
             name = m[0]
-            actions = []
+            parsed["workflows"][name] = {}
+            parsed["workflows"][name]["actions"] = []
             for actions_str in m[1].split(','):
                 m2 = re.search(workflow_action_re, actions_str)
                 if m2:
-                    actions.append({
+                    parsed["workflows"][name]["actions"].append({
                         "category": m2.group(1),
                         "op": m2.group(2),
                         "value": int(m2.group(3)),
                         "next": m2.group(4)
                     })
-                    
-            parsed["workflows"][name] = actions
+                if actions_str and not m2:
+                    parsed["workflows"][name]["default"] = actions_str
 
     for parts_str in data[1].splitlines():
-        part = []
+        part = {}
         for p in parts_str[1:-1].split(','):
             m = re.search(parts_re, p)
             if m:
-                part.append({
-                    "category": m.group(1),
-                    "value": int(m.group(2))
-                })
+                part[m.group(1)] = int(m.group(2))
+
         parsed["parts"].append(part)
 
     return parsed
+
+def apply_rules(part, workflow):
+    # Check if rules applies
+    
+    for action in workflow["actions"]:
+        if action["category"] in part:
+            if action["op"] == '>':
+                if part[action["category"]] > action["value"]:
+                    return action["next"]
+            if action["op"] == '<':
+                if part[action["category"]] < action["value"]:
+                    return action["next"]
+    
+    # Apply default rule      
+    return workflow["default"]
+
 
 def part_a(data):
     data = parse(data)
     total = 0
     
+    for part in data["parts"]:
+        if "in" in data["workflows"]:
+            next = apply_rules(part, data["workflows"]["in"])
+
+            if next == "A":
+                total += part["x"] + part["m"] + part["a"] + part["s"]
+                continue
+            
+            if next == "R":
+                continue
+
+        while next in data["workflows"]:
+            next = apply_rules(part, data["workflows"][next])
+            if next == "A":
+                total += part["x"] + part["m"] + part["a"] + part["s"]
+                break
+            if next == "R":
+                break
+
     return total
 
 
 def part_b(data):
+
     data = parse(data)
     total = 0
 
@@ -81,5 +116,5 @@ test_data_part_b = test_data_part_a
 if __name__ == "__main__":
     data = common.get_data(__file__)
     
-    common.run(part_a, test_data_part_a, data, 0)
+    common.run(part_a, test_data_part_a, data, 19114)
     common.run(part_b, test_data_part_b, data, 0)
